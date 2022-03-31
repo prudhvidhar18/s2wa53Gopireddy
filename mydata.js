1c1bdd707161f859fc9cfc8ca0d4de299edc4a11
prudhvidhar18 <98135740+prudhvidhar18@users.noreply.github.com>
2022-03-30 19:38:45 -0500

    Changes files

--- mydata.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET mydata page. */
-router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Narendar reddy katta',fact: 'I am good at Badminton' });
-});
-
-module.exports = router;
